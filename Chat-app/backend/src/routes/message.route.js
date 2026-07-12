5e61b4efc412ab8f385f06a9521888f5b1025bc3
Tarun Yadav <tarunyadav0838@gmail.com>
2026-07-12 22:49:37 +0530

    message routes optimize

--- Chat-app/backend/src/routes/message.route.js
+++ Chat-app/backend/src/routes/message.route.js
@@ -1,14 +1,20 @@
 import { Router } from "express";
 import { isLoggedIn } from "../middleware/auth.middleware.js";
 import { getAllContacts, getChatUser, getMessageByUserId, sendMessage } from "../controllers/message.controller.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 
 const router = Router();
 
 // message routes
-router.get('/contacts', isLoggedIn,  getAllContacts);
-router.get('/chats',isLoggedIn,  getChatUser);
-router.get('/:id',isLoggedIn ,getMessageByUserId);
-router.post('/send/:id',isLoggedIn, sendMessage);
+router.use(arcjetProtection,isLoggedIn)
+/**
+ * ye upar maine middleware use kiye hai jo phele rate limiter check hoga fir user loggedIn hai ya nhi ye check hoga usek baad routes execute honge
+ */
+
+router.get('/contacts', getAllContacts);
+router.get('/chats', getChatUser);
+router.get('/:id', getMessageByUserId);
+router.post('/send/:id', sendMessage);
 
 export default router;
