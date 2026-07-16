9f04b2624c65872d33c5f5824a0c213e95ce8212
Tarun Yadav <tarunyadav0838@gmail.com>
2026-07-16 16:49:02 +0530

    signup page done ✅

--- Chat-app/frontend/src/pages/SignupPage.jsx
+++ Chat-app/frontend/src/pages/SignupPage.jsx
@@ -1,20 +1,145 @@
 import React, { useState } from 'react'
-import { useAuthStore } from '../store/useAuthStore'
+import {useAuthStore} from '../store/useAuthStore'
+import { Lock, MailIcon, MessageCircleIcon, UserIcon } from 'lucide-react';
+import { LoaderIcon } from 'react-hot-toast';
+import { Link } from 'react-router';
+const SignupPage = () => {
 
-function SignupPage() {
-    // useState hook : yah se mere ko form ka data milega
-    console.log("signup page render")
-    const [formData, setFormData] = useState({fullname:"",email:"",password:""})
+  const [formData, setFormData] = useState({
+    fullname: "",
+    email: "",
+    password: ""
+  })
 
-    // ab iss data ko backend mai bhejna hai uske liye use karenge zustand ka 
-    const {signup , isSigningUp} = useAuthStore();
+  const {signup , isSigningUp , userAuth} = useAuthStore();
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+
+    signup(formData)
+  }
+
 
   return (
-   <div className='w-full flex items-center justify-center p-4 bg-slate-900 '>
-    <div className='relative w-full max-w-6xl md:h-[800px] h-[650px] border border-indigo-800 '>
-        <h1>hello signup</h1>
+    <div className='w-full  flex items-center justify-center p-6 bg-slate-900'>
+      <div
+      className='relative w-full max-w-6xl md:h-[800px] h-[650px] bg-slate-800 rounded-2xl'
+      >
+        <div className=' w-full h-full flex flex-col md:flex-row'>
+
+            {/* left side wala form banayenge  */}
+            
+            <div className='md:w-1/2 h-full p-8 flex items-center justify-center md:border-r border-slate-600/30 '>
+
+                <div className='w-full max-w-md '>
+
+                  {/* HEADING PART DONE  */}
+
+                  <div className='text-center mb-8'>
+                      <MessageCircleIcon  className='w-12 h-12 mx-auto text-slate-400 mb-4' />
+                      <h1 className='text-slate-200 font-bold text-2xl mb-2' >Create Account</h1>
+                      <p className='text-slate-400'>Sign up for new account</p>
+                  </div>
+
+                    {/* FORM PART START  */}
+
+                    <form onSubmit={submitHandler} className='space-y-6'>
+                          {/* full name input  */}
+
+                      <div>
+                        <label className='auth-input-label'>Full Name </label>
+                        <div className='relative'>
+                          <UserIcon className='auth-input-icon' />
+                          <input
+                          value={formData.fullname} 
+                          onChange={(e)=> { setFormData({...formData, fullname: e.target.value}) }}
+                          className='input'
+                          placeholder='tarun yadav'
+                          type="text" />
+                        </div>
+                      </div>
+
+                          {/* email  input  */}
+
+                      <div>
+                        <label className='auth-input-label'>email </label>
+                        <div className='relative'>
+                          <MailIcon className='auth-input-icon' />
+                          <input
+                          value={formData.email} 
+                          onChange={(e)=> { setFormData({...formData, email: e.target.value}) }}
+                          className='input'
+                          placeholder='tarunyadav@example.com'
+                          type="email" />
+                        </div>
+                      </div>
+
+
+                          {/* password  input  */}
+                      <div>
+                        <label className='auth-input-label'>Password </label>
+                        <div className='relative'>
+                          <Lock className='auth-input-icon' />
+                          <input
+                          value={formData.password} 
+                          onChange={(e)=> { setFormData({...formData, password: e.target.value}) }}
+                          className='input'
+                          placeholder='password'
+                          type="password" />
+                        </div>
+                      </div>
+
+                      {/* SUBMIT BUTTON  */}
+                      <button
+                     type='submit'
+                     className='auth-btn'
+                     disabled={isSigningUp}
+                      >
+                        {
+                          isSigningUp ? ( <LoaderIcon className='w-full h-5 mx-auto text-center animate-spin' />  ) : ("Create Account")
+                        }
+
+                      </button>
+
+                    </form>
+
+                    <div className='mt-5 text-center'>
+                      <Link to={'/login'} className='auth-link'> Already have an account? login </Link>
+                    </div>
+
+                </div>
+            </div>
+
+
+            {/* form right side  */}
+              <div
+              className='hidden md:w-1/2 md:flex items-center justify-center p-6 bg-gradient-to-bl from-slate-800/20 to-transparent'
+              >
+
+                  <div>
+                <img
+                  src="/signup.png"
+                  alt="People using mobile devices"
+                  className="w-full h-auto object-contain"
+                />
+                <div className="mt-6 text-center">
+                  <h3 className="text-xl font-medium text-cyan-400">Start Your Journey Today</h3>
+
+                  <div className="mt-4 flex justify-center gap-4 text-lg text-white">
+                    <span className="auth-badge">Free</span>
+                    <span className="auth-badge">Easy Setup</span>
+                    <span className="auth-badge">Private</span>
+                  </div>
+                </div>
+              </div>
+
+              </div>
+
+        </div>
+
+      </div>
+      
     </div>
-   </div>
   )
 }
 
--- Chat-app/frontend/src/store/useAuthStore.js
+++ Chat-app/frontend/src/store/useAuthStore.js
@@ -1,50 +1,49 @@
-import {create} from 'zustand'
+import { create } from 'zustand'
 import axiosInstance from '../lib/axios'
 import { data } from 'react-router';
 import toast from 'react-hot-toast';
 
 // now creating zustand store
 export const useAuthStore = create((set) => ({
-    // yah ab properties / state define karenge 
-    authUser: null,
-    isCheckingAuth: true,
-    isSigningUp: false,
+  // yah ab properties / state define karenge 
+  authUser: null,
+  isCheckingAuth: true,
+  isSigningUp: false,
 
-    checkAuth: async () => {
-        try {
-            const res = await axiosInstance.get('/auth/check')
-            console.log('/check se data agaya');
-            console.log(res.data)
-            console.log(axiosInstance.defaults.baseURL);
-            set({ authUser: res.data })
-        } catch (error) {
-            console.log("error occur in useAuthStore ", error)
-            set({ authUser: null })
-        }
-        finally {
-            set({ isCheckingAuth: false })
-        }
-    },
+  // creating a method for checking user is authenticated or not 
+  checkAuth: async () => {
+    // req send karenge /check par
+    try {
+      const res = await axiosInstance.get('/auth/check');
+      set({ authUser: res.data });
+      console.log("auth user: ", authUser)
 
-    signup: async (data) => {
-        // yah basically hume api call karni hai
-        try {
-            set({isSigningUp: true})
-            const res = await axiosInstance.post("/auth/signup", data);
-            console.log(res.data);
-            set({authUser: res.data});
+    } catch (error) {
+      console.log("Error in check auth : ", error);
+      set({ authUser: null })
 
-            // now sending the success message by taoster
-            toast.success("Account Created Successfully!")
-        } catch (error) {
-            console.log('error occur in signup useAuthStore',error);
-            toast.error(error.response.data.message)
-            
-        }
-        finally{
-            set({isSigningUp: false})
-        }
     }
+    finally {
+      set({ isCheckingAuth: false })
+    }
+  },
+
+  signup: async (data) => {
+    // par user ko signup karane ke liye req send hogi /signup
+    try {
+      set({ isSigningUp: true })
+      const res = await axiosInstance.post('/auth/signup', data)
+      set({ authUser: res.data });
+      toast.success('Account created successfully !')
+    } catch (error) {
+      console.log('error occur in signup authStore', error.message);
+      toast.error(error.response.data.message)
+    }
+    finally {
+      set({ isSigningUp: false })
+    }
+  }
+
 
 
 }))
