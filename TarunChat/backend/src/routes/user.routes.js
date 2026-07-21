cba919cf24e83be92b3521ea3977c5efcb7e67a9
Tarun yadav <97889796+Tarun0838@users.noreply.github.com>
2026-07-21 21:51:25 +0530

    Delete TarunChat directory

--- TarunChat/backend/src/routes/user.routes.js
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-import { registerUser } from "../controller/user.controller.js";
-
-export const userRouter = Router();
-userRouter.route('/register').post(registerUser);
