b2e833be64c2c23deba681a753eee4bb39bdc35a
Tarun Yadav <tarunyadav0838@gmail.com>
2026-07-25 20:05:08 +0530

    signup ui almost done

---
+++ YadavGram/frontend/src/pages/Signup.jsx
@@ -0,0 +1,138 @@
+import React, { useState } from 'react'
+import { Lock, LockIcon, MailIcon, MessageCircleIcon, UserIcon , Eye, EyeOff } from 'lucide-react';
+
+const Signup = () => {
+
+    const [formData, setFormData] = useState({
+        name: "",
+        username: "",
+        email: "",
+        password: ""
+    })
+
+    let [showPassword, setShowPassword] = useState(false)
+
+    return (
+        <div className='w-full h-screen text-white flex flex-col items-center justify-center bg-linear-to-b from-black to-gray-800'>
+            <div className=' w-[90%] lg:max-w-[60%] bg-white rounded-2xl h-162.5 flex items-center justify-center overflow-hidden border-2 border-white '>
+                {/* ab iske andar 2 div banenge  */}
+                <div className='w-full  md:w-1/2 h-full bg-white flex flex-col gap-5 p-2 items-center flex-start '>
+                    {/* HEADER PART  */}
+                    <div className='flex items-center text-black mt-10 gap-3 '>
+                        <span className='text-xl font-semibold '>Sign Up to </span>
+                        <h1 className='text-4xl font-bold font-sans shadow-4xl shadow-gray-200 '>YadavGram</h1>
+                    </div>
+
+                    {/* INPUT PART  */}
+
+                    <form className='space-y-6'>
+
+                        {/* input name part  */}
+                        <div >
+                            <label className='auth-input-label' >Full Name </label>
+
+                            <div className='relative -translate-y-2'>
+                                <UserIcon className='auth-input-icon' />
+                                <input 
+                                className='input '
+                                value={formData.name}
+                                onChange={(e)=> setFormData({...formData , name: e.target.value}) }
+                                type="text"
+                                placeholder='Enter your Name'
+                                />
+                            </div>
+                        </div>
+
+
+                        {/* USERNAME PART  */}
+                        <div >
+                            <label className='auth-input-label' >Username </label>
+
+                            <div className='relative -translate-y-2'>
+                                <UserIcon className='auth-input-icon' />
+                                <input 
+                                value={formData.username}
+                                onChange={(e)=> setFormData({...formData , username: e.target.value})}
+                                className='input '
+                                type="text"
+                                placeholder='Enter your username'
+                                />
+                            </div>
+                        </div>
+
+                            {/* EMAIL PART  */}
+
+                        <div className=' '>
+                            <label className='auth-input-label' >Email </label>
+
+                            <div className='relative -translate-y-2'>
+                                <MailIcon className='auth-input-icon' />
+                                <input 
+                                value={formData.email}
+                                onChange={(e)=> setFormData({...formData , email: e.target.value})}
+                                className='input '
+                                type="email"
+                                placeholder='Enter your email'
+                                />
+                            </div>
+                        </div>
+
+                         {/* password PART  */}
+                        <div className=' '>
+                            <label className='auth-input-label' >Password </label>
+
+                            <div className='relative -translate-y-2 flex flex-col items-center justify-center'>
+                                <LockIcon className='auth-input-icon' />
+                                <input 
+                                value={formData.password}
+                                onChange={(e)=> setFormData({...formData , password: e.target.value})}
+                                className='input '
+                                type={showPassword? "text": "password"}
+                                placeholder='Enter your password'
+                                />
+                                {
+                                    !showPassword? <Eye 
+                                onClick={()=> setShowPassword(true)}
+                                className='show-password-icon'
+                                size={28} color="#c0c0c0" strokeWidth={1.75} /> 
+                                : <EyeOff 
+                                onClick={()=> setShowPassword(false)}
+                                className='show-password-icon'
+                                size={28} color="#c0c0c0" strokeWidth={1.75} />
+                                }
+                                
+                            </div>
+
+                        </div>
+
+                        {/* signUp button part  */}
+
+                        <div className='flex text-slate-900 flex-col items-center justify-center'>
+                            <button
+                            className='auth-btn'
+                            >SignUp</button>
+                          <p 
+                          className='text-lg font-medium '
+                          > Already have an account? 
+                          <span 
+                          className='text-2xl font-semibold ml-2 cursor-pointer'
+                          >Login</span> </p>
+                        </div>
+
+                    </form>
+
+
+
+                </div>
+
+
+
+                <div className='md:w-1/2 h-full lg:flex hidden justify-center items-center flex-col gap-5 text-white text-[16px] font-semibold  rounded-l-[30px] bg-[#000000] shadow-3xl shadow-black'>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Signup
